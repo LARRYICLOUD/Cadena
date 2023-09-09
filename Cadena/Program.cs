b43057afe65c5bf03b3a1ed10716d98f61e35ff9
LARRY MOSQUERA <larrysalsa@icloud.com>
2023-09-08 21:05:57 -0500

    Agregar archivos de proyecto.

---
+++ Cadena/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("YO SOY UN PROGRAMADOR Y EXPERTO EN REDES RECONOCIDO");
+            Console.WriteLine("#JAJAJA_FENOMENAL");
+
+            //Instanciando a String por medio del alias "string"
+            string cadena1 = "";
+            string cadena2 = " ";
+            string cadena3 = "Hola Mundo";
+
+            char[] caracteres = { 'H', 'O', 'L', 'A' };
+            //Creando una instancia de String
+            String cadenaDeCaracteres = new String(caracteres);
+
+            //Imprimiendo instancia
+            Console.WriteLine(cadenaDeCaracteres);
+
+        }
+    }
+}
